4eea22758e63a3337b2dad631b12ae734dd7485d
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-09 02:42:54 +0500

    Finishing Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Amazong Clone</h1>
+     <Header />
     </div>
   );
 }
