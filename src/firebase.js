20198826d1efce1e283b421b3c69fa51fdf4c369
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-09 01:28:00 +0500

    Setting up our project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDr1Mmw5PmESIlOxmoQqtsbb3kpDKlcpH8",
+    authDomain: "with-reactjs.firebaseapp.com",
+    databaseURL: "https://with-reactjs.firebaseio.com",
+    projectId: "with-reactjs",
+    storageBucket: "with-reactjs.appspot.com",
+    messagingSenderId: "25720413418",
+    appId: "1:25720413418:web:44303c8448ab4b29dbcb93",
+    measurementId: "G-R9BGNZYRPT"
+  };
