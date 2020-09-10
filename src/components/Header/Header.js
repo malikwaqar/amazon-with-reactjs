19e43e9badf8790dd770519a1b6449aa877bf376
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-10 06:41:22 +0500

    Logged In email in Header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,7 @@ function Header() {
                 <Link to={!user && '/login'}>
                 <div className="header__option" onClick={handleAuthentication}>
                     <span className="header__optionLineOne">
-                        Hello Guest
+                        Hello {user?.email}
                     </span>
                     <span className="header__optionLineTwo">
                         {user ? 'Sign Out' : 'Sign In'}
